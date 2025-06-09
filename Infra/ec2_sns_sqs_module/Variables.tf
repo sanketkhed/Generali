2b715857addd4ded5ddb4d3ce21675bda4809d7a
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-09 14:36:07 +0530

    Create Variables.tf

---
+++ Infra/ec2_sns_sqs_module/Variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+}
+
+variable "sns_topic_name" {
+  description = "Name of the SNS topic"
+  type        = string
+}
+
+variable "sqs_name" {
+  description = "Name of the SQS queue"
+  type        = string
+}
+
