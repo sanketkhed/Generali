e9426f4bb4f2526fa0b261badea3208b6b4c5525
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-09 18:44:47 +0530

    Update main.tf in module calling

--- Environments/dev/main.tf
+++ Environments/dev/main.tf
@@ -6,12 +6,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "ec2_sns_sqs_module" {
+module "ec2_module" {
   source          = "../../ec2_sns_sqs_module"
   aws_region      = "us-east-1"
   ami_id          = "ami-0abcdef1234567890"
   instance_type   = "t2.micro"
   instance_name   = "dev-ec2"
-  sns_topic_name  = "dev-sns-topic"
+}
+
+module "sns_module"{
+   sns_topic_name  = "dev-sns-topic"
+}
+
+module "sqs_module"{
   sqs_name        = "dev-sqs-queue"
 }
