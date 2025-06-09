856e2cc12f4c6d448fa3025b99a66330554d845c
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-09 19:00:11 +0530

    Update main.tf

--- Environments/prod/main.tf
+++ Environments/prod/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "ec2_module" {
-  source          = "../../ec2_sns_sqs_module"
+  source          = "../../ec2_module"
   aws_region      = "us-west-2"
   ami_id          = "ami-0abcdef1234567890"
   instance_type   = "m5.large"
@@ -18,7 +18,7 @@ module "sqs_module"{
   sqs_name        = "prod-sqs-queue"
 }
 
-module "lambda" {
+module "lambda_module" {
   source          = "../../lambda_module"
   function_name   = "prod-lambda"
   runtime         = "python3.9"
