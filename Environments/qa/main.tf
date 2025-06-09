d1f5c07fa8698b5796f5dd692be449623293fd5a
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-09 15:05:43 +0530

    Created main.tf in qa

---
+++ Environments/qa/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "ec2_sns_sms" {
+  source          = "../../ec2_sns_sms_module"
+  aws_region      = "us-east-2"
+  ami_id          = "ami-0abcdef1234567890"
+  instance_type   = "t3.micro"
+  instance_name   = "qa-ec2"
+  sns_topic_name  = "qa-sns-topic"
+  sqs_name        = "qa-sqs-queue"
+}
