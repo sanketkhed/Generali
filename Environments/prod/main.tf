603ab0ac5de01ac559b485cbf60f3bfb23d6b592
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-09 18:46:24 +0530

    Update main.tf

--- Environments/prod/main.tf
+++ Environments/prod/main.tf
@@ -8,6 +8,12 @@ module "ec2_sns_sqs_module" {
   ami_id          = "ami-0abcdef1234567890"
   instance_type   = "m5.large"
   instance_name   = "prod-ec2"
+}
+
+module "sns_module"{
   sns_topic_name  = "prod-sns-topic"
+}
+
+module "sqs_module"{
   sqs_name        = "prod-sqs-queue"
 }
