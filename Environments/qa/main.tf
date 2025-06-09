a12f916cfca875a139d11901a5529f2c874b24be
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-09 18:48:43 +0530

    Update main.tf

--- Environments/qa/main.tf
+++ Environments/qa/main.tf
@@ -2,12 +2,20 @@ provider "aws" {
   region = "us-east-2"
 }
 
-module "ec2_sns_sqs_module" {
+module "ec2_module" {
   source          = "../../ec2_sns_sqs_module"
   aws_region      = "us-east-2"
   ami_id          = "ami-0abcdef1234567890"
   instance_type   = "t3.micro"
   instance_name   = "qa-ec2"
+  
+  
+}
+
+module "sns_module"{
   sns_topic_name  = "qa-sns-topic"
-  sqs_name        = "qa-sqs-queue"
+}
+
+module "sqs_module"{
+  sqs_name = "qa-sqs-queue"
 }
