bb1e7c7b2f0b99bb2141dad8c773ff649b70e3da
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-09 15:08:53 +0530

    Update main.tf

--- Environments/qa/main.tf
+++ Environments/qa/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-module "ec2_sns_sms" {
+module "ec2_sns_sqs_module" {
   source          = "../../ec2_sns_sms_module"
   aws_region      = "us-east-2"
   ami_id          = "ami-0abcdef1234567890"
