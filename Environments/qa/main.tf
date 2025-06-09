a7133aa5fcb439e5ffaf053b616b888bd1b6a786
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-09 18:58:52 +0530

    Update main.tf

--- Environments/qa/main.tf
+++ Environments/qa/main.tf
@@ -19,3 +19,12 @@ module "sns_module"{
 module "sqs_module"{
   sqs_name = "qa-sqs-queue"
 }
+
+module "lambda" {
+  source          = "../../lambda_module"
+  function_name   = "qa-lambda"
+  runtime         = "python3.9"
+  handler         = "lambda_function.lambda_handler"
+  lambda_package  = "lambda/dev_lambda.zip"
+  sns_topic_arn   = module.ec2_sns_sms.sns_topic_arn
+}
