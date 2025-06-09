37c2fe157390a592d35df9dba7893fb99122c615
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-09 18:34:15 +0530

    Created main.tf in sql module

---
+++ Infra/sqs_module/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+resource "aws_sqs_queue" "queue1" {
+  name = var.sqs_name
+}
