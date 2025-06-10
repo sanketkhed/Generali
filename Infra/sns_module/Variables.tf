204d11686909b42870db07de9f24acd78b6f3594
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-10 12:05:50 +0530

    Update Variables.tf

--- Infra/sns_module/Variables.tf
+++ Infra/sns_module/Variables.tf
@@ -7,3 +7,8 @@ variable "sns_topic_name" {
   description = "Name of the SNS topic"
   type        = string
 }
+variable "enable_sns" {
+  description = "Whether to create an SNS topic"
+  type        = bool
+  default     = false
+}
