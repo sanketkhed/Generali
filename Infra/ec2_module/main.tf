412dbb0ceb899061a9396c1cee1b344cca54f212
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-10 16:30:40 +0530

    Update main.tf

--- Infra/ec2_module/main.tf
+++ Infra/ec2_module/main.tf
@@ -12,20 +12,25 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "instance1" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-
-  tags = merge(
+# Combine all tags dynamically
+locals {
+  dynamic_tags = merge(
+    var.common_tags,
     {
       Name = var.instance_name
     },
-    var.common_tags,
-    var.enable_sqs ? {
+    var.enable_sqs && length(aws_sqs_queue.queue1) > 0 ? {
       SQS_ARN_Tag = aws_sqs_queue.queue1[0].arn
     } : {},
     { for key, value in var.extra_tags : key => value }
   )
+}
+
+# EC2 instance using dynamic tags
+resource "aws_instance" "instance1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags          = local.dynamic_tags
 }
 
 
