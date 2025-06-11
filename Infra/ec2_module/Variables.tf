5e55564ca0cb55161a28a5598ff656b4ec767216
Sanket Khed <36877246+sanketkhed@users.noreply.github.com>
2025-06-11 10:06:56 +0530

    Update Variables.tf

--- Infra/ec2_module/Variables.tf
+++ Infra/ec2_module/Variables.tf
@@ -1,3 +1,9 @@
+variable "tags" {
+  description = "A map of tags to assign to resources"
+  type        = map(string)
+  default     = {}
+}
+
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
